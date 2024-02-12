4a5131e8b332c4b3b4ab8a61b5b7c1e6fb591c57
karlorz <karl.logon@gmail.com>
2024-02-13 06:14:09 +0800

    update

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -2,11 +2,11 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // Construct the target URL with the base URL and the desired path
-  const targetUrl = new URL("/auth/signin", request.nextUrl.origin);
+  // // Construct the target URL with the base URL and the desired path
+  // const targetUrl = new URL("/auth/signin", request.nextUrl.origin);
 
-  // Use NextResponse.rewrite to redirect to the target URL
-  return NextResponse.rewrite(targetUrl);
+  // // Use NextResponse.rewrite to redirect to the target URL
+  // return NextResponse.rewrite(targetUrl);
 }
 
 export const config = {
